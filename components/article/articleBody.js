0ee97831a3652ae1e7ada5adab7389bc26d8a7fe
yqianjiang <y.jiang.psy@gmail.com>
2023-01-26 20:57:15 +0800

    add article display component

---
+++ components/article/articleBody.js
@@ -0,0 +1,129 @@
+import * as React from "react";
+import { findLemma } from "../../lib/lemmatize";
+import styles from "./article.module.scss";
+import clsx from "clsx";
+
+const ArticleBody = ({
+  // 要展示的文章内容
+  paragraphs,
+
+  // 辅助高亮的数据
+  unseenWord,
+  unknownWord,
+  targetWord,
+  translations,
+
+  // 切换显示模式
+  mode,
+  highlightList,
+
+  // handle点击单词的交互
+  toggleTrans,
+  toggleKnown,
+}) => {
+  const computeTranslation = (token) => {
+    // function to compute translation of token
+    token = findLemma(token);
+    const trans = translations[token]?.translation
+      ?.split(".")[1]
+      ?.split(",")[0]
+      ?.split("\\")[0];
+    return mode.showTrans && trans ? `${trans.trim()}` : "";
+  };
+
+  const onClickWord = async (e) => {
+    let token = e.target?.getAttribute("data-token");
+    if (!token) return;
+    token = findLemma(token);
+
+    if (mode.markUnknownWord) {
+      toggleKnown(token);
+    } else if (mode.showTrans) {
+      toggleTrans(token);
+    }
+  };
+
+  // 判断该token后方是否加空格
+  const checkPadding = (sentence, idx) => {
+    const currToken = sentence.tokens[idx];
+    const nextToken = sentence.tokens[idx + 1];
+
+    const notationList = ["(", "'", "‘"];
+
+    if (nextToken === "-") return true;
+    if (notationList.includes(currToken)) return false;
+    if (notationList.includes(nextToken)) return true;
+
+    // 后方连接单词需要加空格
+    if (/\w/.test(nextToken)) return true;
+
+    return false;
+  };
+
+  return (
+    <article className={styles.article}>
+      {paragraphs.map((paragraph, idx) => {
+        return (
+          <p key={`paragraph${idx}`}>
+            {paragraph.map((sentence, idx) => {
+              return (
+                <React.Fragment key={sentence.sentence.slice(0, 5) + idx}>
+                  <span
+                    className={styles["origin-sentence"]}
+                    onClick={onClickWord}
+                  >
+                    {sentence.tokens.map((token, idx) => {
+                      return (
+                        <React.Fragment key={idx}>
+                          <span
+                            data-token={token}
+                            className={clsx([
+                              styles.token,
+                              highlightList.includes("unseen") &&
+                              unseenWord.includes(findLemma(token))
+                                ? styles['token--unseen']
+                                : null,
+                              highlightList.includes("unknown") &&
+                              unknownWord.includes(findLemma(token))
+                                ? styles['token--unknown']
+                                : null,
+                              highlightList.includes("target") &&
+                              !targetWord.includes(findLemma(token))
+                                ? styles['token--ignore']
+                                : null,
+                            ])}
+                          >
+                            {computeTranslation(token) ? (
+                              <ruby>
+                                {token}
+                                <rt>{computeTranslation(token)}</rt>
+                              </ruby>
+                            ) : (
+                              token
+                            )}
+                          </span>
+                          {/* 若下一个词是标点符号，则不必了显示空格了。 */}
+                          {checkPadding(sentence, idx) ? <span> </span> : ""}
+                        </React.Fragment>
+                      );
+                    })}
+                  </span>
+                  <span
+                    className={styles["translate-sentence"]}
+                    style={{
+                      display: mode.showSentenceTrans ? "block" : "none",
+                    }}
+                  >
+                    {sentence.translation}
+                  </span>
+                </React.Fragment>
+              );
+            })}
+          </p>
+        );
+      })}
+    </article>
+  );
+};
+
+export default ArticleBody;
---
+++ components/article/articleControls.js
@@ -0,0 +1,107 @@
+import { Box, Checkbox, Select, Button, FormControlLabel } from "@mui/material";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Chip from "@mui/material/Chip";
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
+const ArticleControls = ({
+  mode,
+  setMode,
+  highlightList,
+  setHighlightList,
+  highlightOptions,
+  highlightOptionsLabels,
+}) => {
+  const handleChangeHighlight = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setHighlightList(
+      // On autofill we get a stringified value.
+      typeof value === "string" ? value.split(",") : value
+    );
+  };
+
+  return (
+    <Box display="flex" flexDirection="column" alignItems="center">
+      <Box display="flex" alignItems="center">
+        <Box mr={1}>显示设置：</Box>
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={mode.showTrans}
+              onChange={() => setMode({ ...mode, showTrans: !mode.showTrans })}
+            />
+          }
+          label="单词翻译"
+        />
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={mode.showSentenceTrans}
+              onChange={() =>
+                setMode({ ...mode, showSentenceTrans: !mode.showSentenceTrans })
+              }
+            />
+          }
+          label="整句翻译"
+        />
+      </Box>
+      <Box display="flex" alignItems="center">
+        <FormControl sx={{ m: 1, width: 300 }}>
+          <InputLabel id="highlight-multiple-chip-label">高亮选项</InputLabel>
+          <Select
+            labelId="highlight-multiple-chip-label"
+            id="highlight-multiple-chip"
+            multiple
+            value={highlightList}
+            onChange={handleChangeHighlight}
+            input={<OutlinedInput id="select-multiple-chip" label="高亮选项" />}
+            renderValue={(selected) => (
+              <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
+                {selected.map((value) => (
+                  <Chip key={value} label={highlightOptionsLabels[value]} />
+                ))}
+              </Box>
+            )}
+            MenuProps={MenuProps}
+          >
+            {highlightOptions.map((option) => (
+              <MenuItem
+                key={option}
+                value={option}
+                // style={getStyles(option, highlightList, theme)}
+              >
+                {highlightOptionsLabels[option]}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </Box>
+      <Box display="flex" alignItems="center">
+        <Button
+          onClick={() =>
+            setMode({ ...mode, markUnknownWord: !mode.markUnknownWord })
+          }
+        >
+          {mode.markUnknownWord ? "停止标记" : "开始标记"}
+        </Button>
+        <Button onClick={() => console.log("更新词表")}>更新词表</Button>
+      </Box>
+    </Box>
+  );
+};
+
+export default ArticleControls;
