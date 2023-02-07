06f97d86e47197f859ae9831aa1f9ff965728f40
yqianjiang <y.jiang.psy@gmail.com>
2023-02-08 00:13:54 +0800

    add Home page

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,32 @@
-import Layout from '../components/Layout'
-import utilStyles from '../styles/utils.module.css'  // using module css with scoped
-import { getSortedPostsData } from '../lib/posts'
-import Link from 'next/link'
-import Date from '../components/date'
-import { useEffect, useState } from 'react'
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Layout from "../components/Layout";
+import utilStyles from "../styles/utils.module.css"; // using module css with scoped
+import { NextLinkComposed } from "../components/Link";
 
 export default function Home() {
-  const [allPostsData, setAllPostsData] = useState([])
-  useEffect(() => {
-    async function fetchData() {
-      const res = await getSortedPostsData()
-      setAllPostsData(res)
-    }
-    fetchData()
-  }, [])
   return (
-    <Layout pageName={'首页'}>
+    <Layout pageName={"首页"}>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>{title}</Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
+        <Typography variant="h2" component="h1" gutterBottom>
+          FitReads 给您量身定制的阅读体验
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          欢迎！想要通过阅读感兴趣的材料提高您的英语水平，却苦于材料太难？FitReads可以通过记录您个人的词汇量、词汇学习目标，帮助您判断材料是否合适，并在您阅读时把对您个人的重要生词高亮出来！
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          请找一篇您感兴趣的文章，马上试用一下吧！
+        </Typography>
+        <Button
+          variant="contained"
+          component={NextLinkComposed}
+          to={{
+            pathname: "add-article/",
+          }}
+        >
+          开始分析！
+        </Button>
       </section>
     </Layout>
-  )
+  );
 }
